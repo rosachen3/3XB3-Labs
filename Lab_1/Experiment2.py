8e23a6b0be378784df665eb25d2d38ef4fd83a7d
rosachen3 <rosa.chen3@hotmail.com>
2023-09-25 11:02:17 -0400

    Implemented optimized selection sort

--- Lab_1/Experiment2.py
+++ Lab_1/Experiment2.py
@@ -27,5 +27,54 @@ def insertion_sort2(L):
             insert(L, i)
     return L
 
-L = create_random_list(10, 20)
-print(insertion_sort2(L))
+#L = create_random_list(10, 20)
+#print(insertion_sort2(L))
+
+
+# Optimized Selection Sort
+
+def selection_sort2(L):
+    i = 0
+    j = len(L) - 1
+    
+    # Run while pointers do not cross each other
+    while(i < j):
+        max_value = L[i]
+        min_value = L[i]
+        
+        max_index = i
+        min_index = i
+        
+        for k in range(i, j + 1):
+            # Search for max value
+            if (L[k] > max_value):
+                max_index = k
+                max_value = L[k]
+
+            # Search for min value
+            elif (L[k] < min_value):
+                min_index = k
+                min_value = L[k]
+          
+        # Swapping min to the right position [sorted <here> ..... sorted]
+        swap(L,i,min_index)
+  
+        # Swapping max item to the right position [sorted ....... <here> sorted]
+        # If max item was swapped to L[min_index] in previous swap,
+        # Swap L[j] and L[min_index]
+        if (L[min_index] == max_value):
+            swap(L,j,min_index)
+        else:
+            swap(L,j,max_index)
+  
+        i += 1
+        j -= 1
+        print("Swapped array", L)
+
+def swap(L, i, j):
+    L[i], L[j] = L[j], L[i]
+
+
+L = create_random_list(8, 30)
+print("Original array: ", L)
+selection_sort2(L)
