889bb2cfe7a1421e7c64146d0af248e2dedf1744
Sylvia Kamel <kamels3@mcmaster.ca>
2023-11-09 22:37:28 -0500

    added ks_brute_force function

--- Lab_3/knapsack.py
+++ Lab_3/knapsack.py
@@ -13,8 +13,22 @@ def randItemSet(numOfItems: int, minWeight: int, maxWeight: int, minValue: int, 
 
 
 def ks_brute_force(items: List[Tuple[int,int]], capacity: int) -> int:
-    
-    return 0
+    highestValue = 0
+    print("Set of Items: ", items)
+    subSets = list(chain.from_iterable(combinations(items, r) for r in range(len(items) + 1)))
+    print("All possible sets: ", subSets)
+    for i in subSets:
+        sumWeight = 0
+        sumValue = 0
+        for j in i:
+            sumWeight += j[0]
+            sumValue += j[1]
+        if sumWeight <= capacity:
+            if sumValue > highestValue:
+                highestValue = sumValue
+    print("highest value is: ", highestValue)
+    return highestValue
+
 def ks_rec(items: List[Tuple[int,int]], capacity: int) -> int:
     
     return 0
@@ -25,3 +39,5 @@ def ks_top_down(items: List[Tuple[int,int]], capacity: int) -> int:
 
     return 0
 
+#### TESTING CODE ####
+# ks_brute_force(randItemSet(10, 2, 17, 100, 200), 20)
