11019d13c1e9bcf7b4e77142a2da13d63845a66f
rosachen3 <rosa.chen3@hotmail.com>
2023-12-04 20:07:04 -0500

    Deleted old comments

--- Final _Project/final_project_part1.py
+++ Final _Project/final_project_part1.py
@@ -243,31 +243,4 @@ def bellman_ford_approx(G, source, k):
                     pred[neighbour] = node
     return dist
 
-# ************* Part 1 - Testing code for Shortest Path Approximations *************
-
-# def dijkstra_graph1(numNodesList, numRandomGraphs):
-#     numK = []
-#     distances = []
-
-#     for numNode in numNodesList: 
-#         maxK = numNode-1
-#         G = create_random_complete_graph(numNode, 20)
-
-#         for k in range(0, maxK, 5):       
-#             G_copy = G.copy()
-#             numK.append(k)
-#             # Generate a certain number of random graphs for each test
-#             distances.append(total_dist(dijkstra_approx(G_copy, 0, k)))
-
-    
-#         plot.plot(distances, numK, label=f"Graph With {numNode} Nodes")
-#     plot.xlabel('Number of Nodes')
-#     plot.ylabel('Number k')
-#     plot.title('Nodes vs. Number k')
-#     plot.legend()
-#     plot.show()
-
-# numNodesList = [5,10,15]
-# numRandomGraphs = 5  # Number of random graphs to create for each test
-# dijkstra_graph1(numNodesList, numRandomGraphs)
 
